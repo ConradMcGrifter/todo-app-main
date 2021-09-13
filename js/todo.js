7de2616100185e666fe52debab384636f7bbf963
skf <conradmcgrifter@protonmail.com>
2021-09-13 17:23:05 -0400

    working on the js for creating the todo items
    still need to add control  panel to filter completed / active todos

--- js/todo.js
+++ js/todo.js
@@ -10,23 +10,36 @@ const TODO_LIST = document.querySelector(".todo-list");
 // });
 
 const createTodo = () => {
+    if (TODO_INPUT.value == "") {
+        return;
+    }
+
     const TODO_ITEM = document.createElement("div");
     const CHECKBOX = document.createElement("span");
     const TODO_TEXT = document.createElement("p");
+    const TODO_CLOSE = document.createElement("span");
 
     TODO_ITEM.classList.add("todo-item");
     CHECKBOX.classList.add("checkbox");
     TODO_TEXT.classList.add("todoText");
+    TODO_CLOSE.classList.add("close");
+
     TODO_TEXT.innerText = TODO_INPUT.value;
 
     TODO_ITEM.append(CHECKBOX);
     TODO_ITEM.append(TODO_TEXT);
+    TODO_ITEM.append(TODO_CLOSE);
     TODO_LIST.append(TODO_ITEM);
 
     TODO_INPUT.value = "";
 
     CHECKBOX.addEventListener("click", () => {
         CHECKBOX.classList.toggle("toggleCheck");
+        TODO_TEXT.classList.toggle("completed");
+    });
+
+    TODO_CLOSE.addEventListener("click", () => {
+        TODO_ITEM.remove();
     });
 };
 
