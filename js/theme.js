418a0a2e6186d02f0e9d9c10d3d9731425757169
skf <conradmcgrifter@protonmail.com>
2021-09-12 20:10:53 -0400

    finished testing colors

--- js/theme.js
+++ js/theme.js
@@ -1,7 +1,11 @@
 const BODY = document.body;
-
 const TEST_BTN = document.querySelector(".gaah");
 
 const myTestFunc = () => {
     BODY.classList.toggle("dark-mode");
 };
+
+TEST_BTN.addEventListener("click", () => {
+    myTestFunc();
+    console.log("test");
+});
