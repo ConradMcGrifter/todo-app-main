29051fbc9fe379e3a80b3ba3f14bfa8cc0e3979b
skf <conradmcgrifter@protonmail.com>
2021-09-16 15:55:02 -0400

    update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,6 +1,10 @@
 const THEME_TOGGLE = document.querySelector("#theme-toggle");
 let darkMode = localStorage.getItem("darkMode");
 
+if (darkMode === "enabled") {
+    document.body.classList.add("dark-mode");
+}
+
 const toggleTheme = () => {
     const BODY = document.body;
     const MOON = document.querySelector(".moon");
@@ -14,14 +18,10 @@ const toggleTheme = () => {
 
     if (BODY.classList.contains("dark-mode")) {
         localStorage.setItem("darkMode", "enabled");
-    } else if (!BODY.classList.contains("dark-mode")) {
+    } else {
         localStorage.setItem("darkMode", null);
     }
 };
-
-if (darkMode === "enabled") {
-    document.body.classList.add("dark-mode");
-}
 
 THEME_TOGGLE.addEventListener("click", () => {
     toggleTheme();
