bd529e36a79a81c97de3679b2f6bd332c36d02df
skf <conradmcgrifter@protonmail.com>
2021-09-20 17:18:12 -0400

    working on local storage for active todo items

--- js/todo.js
+++ js/todo.js
@@ -95,7 +95,6 @@ const filterActive = () => {
     setItemVisibility(globalCompletedArr, globalActiveArr);
 
     alertEmptyList(ACTIVE_FILTER, globalActiveArr, "No active tasks");
-    console.log("test");
 };
 //-------------------------------------------------------------------------
 
@@ -135,7 +134,156 @@ const showAll = () => {
     }
 };
 //-------------------------------------------------------------------------
+const saveLocalStorage = (todoText) => {
+    let task = todoText;
 
+    //check if there are any tasks saved in local storage, if not --> make an empty array
+    if (localStorage.getItem("activeTasks") === null) {
+        localStorage.setItem("activeTasks", "[]");
+    }
+
+    // get the tasks saved in local storage
+    let saved_tasks = JSON.parse(localStorage.getItem("activeTasks"));
+
+    //add the new task to saved_tasks array
+    saved_tasks.push(task);
+
+    //save the tasks list to local storage
+    localStorage.setItem("activeTasks", JSON.stringify(saved_tasks));
+};
+//-------------------------------------------------------------------------
+
+const removeFromLocalStorage = (todoText) => {
+    let task = todoText;
+
+    let saved_tasks = JSON.parse(localStorage.getItem("activeTasks"));
+
+    let taskIndex = saved_tasks.indexOf(task);
+
+    saved_tasks.splice(taskIndex, 1);
+
+    localStorage.setItem("activeTasks", JSON.stringify(saved_tasks));
+    console.log(taskIndex);
+};
+//-------------------------------------------------------------------------
+
+const getTodos = () => {
+    if (
+        localStorage.getItem("activeTasks") === "[]" ||
+        localStorage.getItem("activeTasks") === null
+    ) {
+        return;
+    }
+
+    TODO_CONTAINER.classList.add("active");
+
+    let savedTasks = JSON.parse(localStorage.getItem("activeTasks"));
+
+    savedTasks.forEach((task) => {
+        // create elements to be added to the DOM
+        const TODO_ITEM = document.createElement("li");
+        const CHECKBOX = document.createElement("span");
+        const TODO_TEXT = document.createElement("p");
+        const TODO_CLOSE = document.createElement("span");
+
+        // add classes and set inner text
+        TODO_ITEM.classList.add("todo-item");
+        CHECKBOX.classList.add("checkbox");
+        TODO_TEXT.classList.add("todoText");
+        TODO_CLOSE.classList.add("close");
+        TODO_TEXT.innerText = task;
+
+        // append elements
+        TODO_ITEM.append(CHECKBOX);
+        TODO_ITEM.append(TODO_TEXT);
+        TODO_ITEM.append(TODO_CLOSE);
+        TODO_LIST.append(TODO_ITEM);
+
+        // 🔔 event listener for the circle checkbox
+        CHECKBOX.addEventListener("click", () => {
+            CHECKBOX.classList.toggle("toggleCheck");
+            TODO_TEXT.classList.toggle("completed");
+            TODO_ITEM.classList.toggle("completed");
+
+            if (ACTIVE_FILTER.classList.contains("selected")) {
+                filterActive();
+            }
+
+            if (COMPLETED_FILTER.classList.contains("selected")) {
+                filterCompleted();
+            }
+
+            if (CHECKBOX.classList.contains("toggleCheck")) {
+                removeFromLocalStorage(TODO_TEXT.innerText);
+            } else {
+                saveLocalStorage(TODO_TEXT.innerText);
+            }
+            setGlobalArrayValues();
+            displayTasksLeft();
+        });
+
+        // 🔔 event listener for the todo-text --> it is the same code as the event listener for checkbox
+        TODO_TEXT.addEventListener("click", () => {
+            CHECKBOX.classList.toggle("toggleCheck");
+            TODO_TEXT.classList.toggle("completed");
+            TODO_ITEM.classList.toggle("completed");
+
+            if (ACTIVE_FILTER.classList.contains("selected")) {
+                filterActive();
+            }
+
+            if (COMPLETED_FILTER.classList.contains("selected")) {
+                filterCompleted();
+            }
+
+            setGlobalArrayValues();
+
+            displayTasksLeft();
+        });
+
+        // 🔔 event listener for the "X" close button
+        TODO_CLOSE.addEventListener("click", () => {
+            //this animation runs before the item is rmoved
+            TODO_ITEM.style.animation = "fadeOut .25s ease";
+
+            //this code runs after 350 milliseconds so that the animation has time to play
+            setTimeout(function () {
+                TODO_ITEM.remove();
+                setGlobalArrayValues();
+
+                if (ACTIVE_FILTER.classList.contains("selected")) {
+                    filterActive();
+                }
+
+                if (COMPLETED_FILTER.classList.contains("selected")) {
+                    filterCompleted();
+                }
+
+                displayTasksLeft();
+
+                //hide the entire todo-container if there are no completed or active tasks left
+                if (
+                    globalCompletedArr.length === 0 &&
+                    globalActiveArr.length === 0
+                ) {
+                    TODO_CONTAINER.classList.remove("active");
+                }
+            }, 250);
+
+            removeFromLocalStorage(TODO_TEXT.innerText);
+        });
+    });
+
+    setGlobalArrayValues();
+
+    //display task count
+    document.querySelector(
+        ".itemsLeft"
+    ).innerText = `${savedTasks.length} items left`;
+
+    console.log(savedTasks);
+};
+//-------------------------------------------------------------------------
 const createTodo = () => {
     if (TODO_INPUT.value == "") {
         INPUT_WRAP.style.setProperty("--opacity", 1);
@@ -168,8 +316,6 @@ const createTodo = () => {
     TODO_ITEM.append(TODO_CLOSE);
     TODO_LIST.append(TODO_ITEM);
 
-    TODO_INPUT.value = ""; // reset input field to blank after user creates to-do item
-
     setGlobalArrayValues();
 
     // when the user creates a todo item, if the filter by active option is selected and the number of active items is greater than 0 --> remove the display class from the "empty list warning" element
@@ -197,8 +343,13 @@ const createTodo = () => {
             filterCompleted();
         }
 
-        setGlobalArrayValues();
+        if (CHECKBOX.classList.contains("toggleCheck")) {
+            removeFromLocalStorage(TODO_TEXT.innerText);
+        } else {
+            saveLocalStorage(TODO_TEXT.innerText);
+        }
 
+        setGlobalArrayValues();
         displayTasksLeft();
     });
 
@@ -249,10 +400,15 @@ const createTodo = () => {
                 TODO_CONTAINER.classList.remove("active");
             }
         }, 250);
+
+        removeFromLocalStorage(TODO_TEXT.innerText);
     });
 
+    saveLocalStorage(TODO_TEXT.innerText);
+
+    TODO_INPUT.value = ""; // reset input field to blank after user creates to-do item
     displayTasksLeft();
-}; // createTodo() function end
+};
 
 // -------------------🔔 🔔 EVENT LISTENERS🔔 🔔-------------------
 
@@ -290,9 +446,11 @@ COMPLETED_FILTER.addEventListener("click", filterCompleted);
 
 ALL_FILTER.addEventListener("click", showAll);
 
-// 🔻 on input --> if input value != an empty string --> hide tooltip
+// 🔻 on input --> if input value does not equal an empty string --> hide tooltip
 TODO_INPUT.addEventListener("input", () => {
     if (TODO_INPUT.value != "") {
         INPUT_WRAP.style.setProperty("--opacity", 0);
     }
 });
+
+getTodos();
